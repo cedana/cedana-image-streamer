1f26d1e7da332e1a40588cdf3d8bfd345cc257ff
Liana Koleva <43767763+lianakoleva@users.noreply.github.com>
2024-12-02 22:11:57 +0000

    clean up unused

--- scripts/ci/helpers.sh
+++ scripts/ci/helpers.sh
@@ -8,7 +8,7 @@
 ##
 
 APT_PACKAGES="wget git make curl libnl-3-dev libnet-dev \
-    libbsd-dev runc libcap-dev libgpgme-dev \
+    libbsd-dev libcap-dev libgpgme-dev \
     btrfs-progs libbtrfs-dev libseccomp-dev libapparmor-dev \
     libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler \
     protobuf-compiler python3-protobuf software-properties-common \
@@ -22,46 +22,6 @@ install_apt_packages() {
     done
 }
 
-
-install_bats_core() {
-    git clone https://github.com/bats-core/bats-core.git
-    pushd bats-core
-    ./install.sh /usr/local
-    popd && rm -rf bats-core
-}
-
-install_docker() {
-    sudo apt-get update
-    sudo apt-get install ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-}
-
-
-install_buildah() {
-    sudo apt-get update
-    sudo apt-get -y install buildah
-}
-
-install_crictl() {
-    VERSION="v1.30.0"
-    curl -L https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-${VERSION}-linux-amd64.tar.gz --output crictl-${VERSION}-linux-amd64.tar.gz
-    sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
-    rm -f crictl-$VERSION-linux-amd64.tar.gz
-
-}
-
-install_otelcol_contrib() {
-    wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.94.0/otelcol-contrib_0.94.0_linux_amd64.deb
-    dpkg-deb -x otelcol-contrib_0.94.0_linux_amd64.deb extracted/ && cp extracted/usr/bin/otelcol-contrib /usr/bin/otelcol-contrib
-}
 print_header() {
     echo "############### $1 ###############"
 }
@@ -102,44 +62,8 @@ setup_ci_build() {
     install_apt_packages
 }
 
-setup_ci() {
-    setup_ci_build
-    install_bats_core
-
-    install_docker
-    install_sysbox
-    install_otelcol_contrib
-    install_buildah
-    install_crictl
-
-    wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz && rm -rf /usr/local/go
-    tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz && rm go1.22.0.linux-amd64.tar.gz
-    mkdir -p $HOME/.cedana
-    echo '{"client":{"leave_running":false, "task":""}, "connection":{"cedana_url": "https://ci.cedana.ai"}}' > $HOME/.cedana/client_config.json
-
-    # Set GOPATH and update PATH
-    echo "export GOPATH=$HOME/go" >> /etc/environment
-    echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin:$GOPATH/bin" >> /etc/environment
-    echo "export CEDANA_URL=https://ci.cedana.ai" >> /etc/environment
-
-    # Install CRIU
-    TAG=latest
-    curl -1sLf -O https://dl.cloudsmith.io/$CLOUDSMITH_ENTITLEMENT_TOKEN_CRIU/cedana/criu/raw/versions/$TAG/criu
-    chmod +x criu
-    sudo cp criu /usr/local/sbin/
-
-    # Install cedana-image-streamer
-    curl -1sLf -O https://dl.cloudsmith.io/$CLOUDSMITH_ENTITLEMENT_TOKEN_STREAMER/cedana/cedana-image-streamer/raw/versions/$TAG/cedana-image-streamer
-    chmod +x cedana-image-streamer
-    sudo cp cedana-image-streamer /usr/bin/
-}
-
 source_env() {
     source /etc/environment
-}
-
-start_otelcol() {
-    otelcol-contrib --config test/benchmarks/otelcol-config.yaml &
 }
 
 start_cedana() {
